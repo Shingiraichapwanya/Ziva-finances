d097b5a4616a5bb9c44dc5ded2fcc7feb1be5ba8
Shingiraichapwanya <shingiraichapwanya@gmail.com>
2026-09-07 21:13:52 +0000

    Deploying to gh-pages from @ Shingiraichapwanya/Ziva-finances@6a444b157f82648768f684973823af5f624cc37c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2194778109" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2543379137" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
