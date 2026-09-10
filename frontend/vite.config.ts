2bfe1e11df6ce5c84a1ac017c6cef6bfabcc4452
Shingiraichapwanya <shingiraichapwanya@gmail.com>
2026-09-10 17:00:52 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,21 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: '/Ziva-finances/',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_RENDER_URL || 'http://localhost:3001',
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
